588dcd929289f62aff86d47ce960195ad37cf546
Lucas Gontijo <lucasgontijocft@gmail.com>
2023-06-02 16:34:08 -0300

    list-queue-stack

---
+++ DataStructure/Dynamic_Template_List-Queue-Stack/src/methods.hpp
@@ -0,0 +1,27 @@
+#ifndef METHODS_HPP 
+#define METHODS_HPP
+
+#include <iostream>
+using namespace std;
+
+template <typename T>
+struct Node {
+    T value;
+    Node<T>* next;
+};
+
+template <typename T>
+class Sequence {
+private:
+    Node<T>* begin;
+    Node<T>* end;
+public:
+    Sequence();
+    void Insert(T value);
+    void Print();
+    T queueLeave();
+    T stackPop();
+    T listRemove(T value);
+};
+
+#endif
